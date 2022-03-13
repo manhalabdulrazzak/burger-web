65b5340536160b70188e657606a5a170b6b09a26
manhal abdulraazak <zedalfares@gmail.com>
2022-03-14 00:50:13 +0100

    easyadmin-bundle

---
+++ migrations/Version20220313233620.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220313233620 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE product (id INT AUTO_INCREMENT NOT NULL, product VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, favorite_count INT DEFAULT NULL, price VARCHAR(255) DEFAULT NULL, image_url VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('DROP TABLE products');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE products (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('DROP TABLE product');
+    }
+}
---
+++ src/Entity/Products.php
@@ -0,0 +1,265 @@
+<?php
+
+
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+
+
+/**
+
+ * @ORM\Entity(repositoryClass="App\Repository\ProductRepository")
+
+ */
+
+class Product
+
+{
+
+    /**
+
+     * @ORM\Id()
+
+     * @ORM\GeneratedValue()
+
+     * @ORM\Column(type="integer")
+
+     */
+
+    private $id;
+
+
+
+    /**
+
+     * @ORM\Column(name="product", type="string", length=255)
+
+     */
+
+    private $product;
+
+
+
+    /**
+
+     * @ORM\Column(name="description", type="string", length=255)
+
+     */
+
+    private $description;
+
+
+
+    /**
+
+     * @ORM\Column(name="favorite_count", type="integer", nullable=true)
+
+     */
+
+    private $favoriteCount;
+
+
+
+    /**
+
+     * @ORM\Column(name="price", type="string", length=255, nullable=true)
+
+     */
+
+    private $price;
+
+
+
+    /**
+
+     * @ORM\Column(name="image_url", type="string", length=255, nullable=true)
+
+     */
+
+    private $imageUrl;
+
+
+
+    /**
+
+     * @return mixed
+
+     */
+
+    public function getId()
+
+    {
+
+        return $this->id;
+
+    }
+
+
+
+
+
+    /**
+
+     * @return mixed
+
+     */
+
+    public function getProduct()
+
+    {
+
+        return $this->product;
+
+    }
+
+
+
+    /**
+
+     * @param mixed $product
+
+     */
+
+    public function setProduct($product): void
+
+    {
+
+        $this->product = $product;
+
+    }
+
+
+
+
+
+    /**
+
+     * @return mixed
+
+     */
+
+    public function getDescription()
+
+    {
+
+        return $this->description;
+
+    }
+
+
+
+    /**
+
+     * @param mixed $description
+
+     */
+
+    public function setDescription($description): void
+
+    {
+
+        $this->description = $description;
+
+    }
+
+
+
+
+
+    /**
+
+     * @return mixed
+
+     */
+
+    public function getFavoriteCount()
+
+    {
+
+        return $this->favoriteCount;
+
+    }
+
+
+
+    /**
+
+     * @param mixed $favoriteCount
+
+     */
+
+    public function setFavoriteCount($favoriteCount): void
+
+    {
+
+        $this->favoriteCount = $favoriteCount;
+
+    }
+
+
+
+    /**
+
+     * @return mixed
+
+     */
+
+    public function getPrice()
+
+    {
+
+        return $this->price;
+
+    }
+
+
+
+    /**
+
+     * @param mixed $price
+
+     */
+
+    public function setPrice($price): void
+
+    {
+
+        $this->price = $price;
+
+    }
+
+
+
+    /**
+
+     * @return mixed
+
+     */
+
+    public function getImageUrl()
+
+    {
+
+        return $this->imageUrl;
+
+    }
+
+
+
+    /**
+
+     * @param mixed $imageUrl
+
+     */
+
+    public function setImageUrl($imageUrl): void
+
+    {
+
+        $this->imageUrl = $imageUrl;
+
+    }
+
+}
